2c925d5afa314bfe7c022f5570dc0379aaea6fe9
lzwink <1217515511@qq.com>
2019-10-31 22:22:50 +0800

    update: 未完成功能存档

--- models/users.go
+++ models/users.go
@@ -142,20 +142,15 @@ func (u *Users) UpdateUserPwd(userId int, userName string, oldPwd string, newPwd
 }
 
 // 批量生成用户账户信息
-//func (u *Users) CreateUsersList() {
-//	userName := u.RandomStr(6)
-//	pwd := u.RandomStr(6)
-//
-//
-//}
-//
-//func (u *Users) RandomStr(l int) string {
-//	str := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//	bytes := []byte(str)
-//	result := make([]byte, 0)
-//	r := rand2.New(rand2.NewSource(time.Now().UnixNano()))
-//	for i := 0; i < l; i++ {
-//		result = append(result, bytes[r.Intn(len(bytes))])
-//	}
-//	return string(result)
-//}
+func (u *Users) CreateUsersList() {}
+
+func (u *Users) RandomStr(l int) string {
+	str := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := make([]byte, 0)
+	r := rand2.New(rand2.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
